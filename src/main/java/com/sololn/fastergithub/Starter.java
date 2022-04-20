11a3426800c2ff0c16a023998e27af597e3be7ff
何国健 <1664931260@qq.com>
2022-04-20 16:14:45 +0800

    【功能问题】 切换源为api.k780.com 【提交说明】 切换源为api.k780.com 【提交人】 何国健
    Signed-off-by: 何国健 <1664931260@qq.com>

--- src/main/java/com/sololn/fastergithub/Starter.java
+++ src/main/java/com/sololn/fastergithub/Starter.java
@@ -10,19 +10,22 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 /**
- * @ClassName starter
- * @Description springboot 启动时执行
- * @Author HeGuojian
- * @Date 2021/8/30 17:02
- * @Version 1.0
- **/
+ ** springboot 启动时执行
+ * */
 @Component
 public class Starter implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-        String url  = "https://myssl.com/api/v1/tools/dns_query?qtype=1&host=";
-        String params = "qmode=-1";
+        //String url  = "https://myssl.com/api/v1/tools/dns_query?qtype=1&host=";
         Path outPath = Paths.get(System.getProperty("user.dir") ,"hosts");
+        String queryUrl = IpUtil.readRequestUrlFromDom("https://www.nowapi.com/api/dns.a");
+        queryUrl = queryUrl.replaceAll("&amp;", "&");
+        // http://api.k780.com/?app=dns.a&domain=www.qq.com&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json
+        int start = queryUrl.indexOf("&");
+        int end = queryUrl.indexOf("&", start + 1);
+        String url = queryUrl.substring(0, start) + "&domain=";
+        String params = queryUrl.substring(end + 1 );
+        //Map<String, String> stringStringMap = IpUtil.readIpMap(url, params);
         
         Map<String, String> stringStringMap = IpUtil.readIpMap(url, params);
         HostUtil.toFile(stringStringMap, outPath);
--- src/main/java/com/sololn/fastergithub/util/IpUtil.java
+++ src/main/java/com/sololn/fastergithub/util/IpUtil.java
@@ -1,9 +1,15 @@
 package com.sololn.fastergithub.util;
 
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -35,25 +41,41 @@ public class IpUtil {
         Map<String, String> ipMap = new HashMap<>();
         List<String> error = new ArrayList<>();
         sources.forEach(u -> {
-            try {
-                StringBuilder url = new StringBuilder();
-                url.append(queryUrl).append(u).append("&");
-                Optional<String> reduce = Arrays.stream(params).reduce((x, y) -> x +
-                        "&" + y);
-                reduce.ifPresent(url::append);
-                String res = Inet4Address.sendGet(url.toString());
-                String ipFromJson = JsonUtil.getIpFromJson(res);
-                if (StringUtils.isEmpty(ipFromJson)){
-                    log.info("source {} there is no available ip", u);
-                    error.add(u);
-                    return;
-                }
-                ipMap.put(u, ipFromJson);
-            } catch (IOException e) {
-                log.error(e.getMessage());
+            StringBuilder url = new StringBuilder();
+            url.append(queryUrl).append(u).append("&");
+            Optional<String> reduce = Arrays.stream(params).reduce((x, y) -> x +
+                    "&" + y);
+            reduce.ifPresent(url::append);
+            String res = Inet4Address.sendGet(url.toString());
+            //String ipFromJson = JsonUtil.getIpFromJson(res);
+            JSONObject result = JSONObject.parseObject(res).getJSONObject("result");
+            String a = result.getString("a");
+            String ipFromJson = a.split(",")[0];
+            if (StringUtils.isEmpty(ipFromJson)){
+                log.info("source {} there is no available ip", u);
+                error.add(u);
+                return;
             }
+            ipMap.put(u, ipFromJson);
         });
+        log.error("error: {}",error.toString());
         return ipMap;
     }
     
+    public static String readRequestUrlFromDom(String url) {
+        try {
+            URL realUrl = new URL(url);
+            Document document = Jsoup.parse(realUrl, 10000);
+            Elements elements = document.select("a");
+            Elements elementsByAttributeValue = document.getElementsByAttributeValue("target", "_blank");
+            for (Element element : elementsByAttributeValue) {
+                if (element.attr("href").contains("api.k780.com")) {
+                    return   element.html();
+                }
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return "";
+    }
 }
